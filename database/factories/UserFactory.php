5465738e762e863ab1d35d5d2186bd1e7ea3432a
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-01-17 16:16:44 +0700

    refactor(user): improve factory to generate sequential users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,19 +17,42 @@ class UserFactory extends Factory
     protected static ?string $password;
 
     /**
+     * Counter for generating sequential user numbers
+     */
+    protected static int $totalCount = 0;
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
     public function definition(): array
     {
+        self::$totalCount++;
+
+        $paddingLength = strlen((string) $this->count);
+        $paddedSequence = str_pad((string) self::$totalCount, $paddingLength, '0', STR_PAD_LEFT);
+
+        $name = "User-{$paddedSequence}";
+        $email = "user-{$paddedSequence}@example.com";
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $name,
+            'email' => $email,
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => Hash::make(static::$password ?? 'password'),
             'remember_token' => Str::random(10),
         ];
+    }
+
+    /**
+     * Set the password that should be used for the users.
+     */
+    public function password(string $password): static
+    {
+        static::$password = $password;
+
+        return $this;
     }
 
     /**
