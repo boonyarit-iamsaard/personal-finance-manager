e479de1d67ade89fce9c7caa5fce3d48ec18f65b
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-01-17 12:34:24 +0700

    ci: remove unnecessary lint-staged exclusion

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 export default {
-    '**/*.php,!_ide_helper_models.php,!_ide_helper.php,!.phpstorm.meta.php': ['vendor/bin/duster lint'],
+    '**/*.php,!_ide_helper_models.php,!_ide_helper.php': ['vendor/bin/duster lint'],
     '**/*': 'prettier --check --ignore-unknown',
 };
