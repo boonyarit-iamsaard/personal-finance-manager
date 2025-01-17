af82ad75f04a06c693bede99f5599a44ae595860
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-01-17 15:46:48 +0700

    chore: add eslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 export default {
     '**/*.php,!_ide_helper_models.php,!_ide_helper.php': ['vendor/bin/duster lint'],
     '**/*': 'prettier --check --ignore-unknown',
+    '**/*.js': ['eslint'],
 };
